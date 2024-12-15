eef8b457eafc17bc756f7dc485240c9edf34e7e4
kisui <63273180+kisuisou@users.noreply.github.com>
2024-12-15 21:48:49 +0900

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    const { children } = $props();
+</script>
+
+<div class="footer-fixed">
+    <nav class="navbar bg-light">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand my-navbar-brand">Amaryllis</a>
+        </div>
+    </nav>
+
+    <div class="container">
+        {@render children()}
+    </div>
+
+    <footer class="footer bg-light">
+        <div class="container-fluid">
+           <span class="text-secondary">Powered by <a href="https://kit.svelte.dev/">SvelteKit</a> </span>
+        </div>
+    </footer>
+</div>
+
+<style>
+    .my-navbar-brand{
+        color: #DB7093;
+        font-size: 1.6rem;
+    }
+    .footer-fixed {
+        min-height: 100vh;
+        position: relative;
+        padding-bottom: 150px;
+        box-sizing: border-box;
+    }
+
+    footer {
+        position: absolute;
+        bottom: 0;
+    }
+</style>
