ba49673b983e75ee5afd59aee3877428983aa48d
kisui <63273180+kisuisou@users.noreply.github.com>
2024-12-26 21:21:08 +0900

    ログイン画面を実装

---
+++ src/routes/signin/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+
+</script>
+
+
+<form class="mx-auto w-50">
+    <h2>SignIn</h2>
+    <div class="mb-3">
+      <label for="user_id_input" class="form-label">User ID</label>
+      <input type="text" class="form-control" id="user_id_input">
+    </div>
+    <div class="mb-3">
+      <label for="password_input" class="form-label">Password</label>
+      <input type="password" class="form-control" id="password_input">
+    </div>
+    <button type="submit" class="btn btn-primary my-submit-btn">Submit</button>
+</form>
+
+
+<style>
+    .my-submit-btn{
+        background-color: #DB7093;
+        border-color: #DB7093;
+    }
+</style>
