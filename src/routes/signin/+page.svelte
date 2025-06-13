1b6ee4cfc3ee826fbd93c40d6e05878dfc85292d
kisui <63273180+kisuisou@users.noreply.github.com>
2025-06-13 15:28:15 +0000

    svelte5対応

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,54 +1,70 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
-    import { PUBLIC_API_ORIGIN } from "$env/static/public";
-    import { user_id } from "../../stores";
+  import { goto } from "$app/navigation";
+  import { PUBLIC_API_ORIGIN } from "$env/static/public";
+  import { user_id } from "../../stores";
 
-  let req = {"user_id": "", "password": ""}
-  
+  let req = $state({ user_id: "", password: "" });
+
   const signin = async () => {
-    const signin_result =  await fetch(PUBLIC_API_ORIGIN + "/signin", {
-     method: "POST" ,
-     mode: "cors",
-     credentials: "include",
-     headers: {
-      "Content-Type": "application/json"
-     },
-      body: JSON.stringify(req)
+    const signin_result = await fetch(PUBLIC_API_ORIGIN + "/signin", {
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req),
     });
-    if(signin_result.status == 200){
-      $user_id = req.user_id
-      goto("/book-shelf")
+    if (signin_result.status == 200) {
+      $user_id = req.user_id;
+      goto("/book-shelf");
     }
-  }
+  };
 
-  let promise: Promise<void>
+  let promise: Promise<void> | undefined = $state();
 </script>
 
-
 <form class="mx-auto w-50">
-    <h2 class="mt-2">SignIn</h2>
-    <div class="mb-3">
-      <label for="user_id_input" class="form-label">User ID</label>
-      <input type="text" class="form-control" id="user_id_input" bind:value={req.user_id}>
-    </div>
-    <div class="mb-3">
-      <label for="password_input" class="form-label">Password</label>
-      <input type="password" class="form-control" id="password_input" bind:value={req.password}>
-    </div>
-    <button type="submit" class="btn btn-primary my-submit-btn" on:click={() => promise = signin()}>
-      {#await promise}
-        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-      {:then _} 
-        Submit
-      {/await}
-    </button>
+  <h2 class="mt-2">SignIn</h2>
+  <div class="mb-3">
+    <label for="user_id_input" class="form-label">User ID</label>
+    <input
+      type="text"
+      class="form-control"
+      id="user_id_input"
+      bind:value={req.user_id}
+    />
+  </div>
+  <div class="mb-3">
+    <label for="password_input" class="form-label">Password</label>
+    <input
+      type="password"
+      class="form-control"
+      id="password_input"
+      bind:value={req.password}
+    />
+  </div>
+  <button
+    type="submit"
+    class="btn btn-primary my-submit-btn"
+    onclick={() => (promise = signin())}
+  >
+    {#await promise}
+      <span
+        class="spinner-border spinner-border-sm"
+        role="status"
+        aria-hidden="true"
+      ></span>
+    {:then _}
+      Submit
+    {/await}
+  </button>
 </form>
 
-
 <style>
-    .my-submit-btn{
-        background-color: #DB7093;
-        border-color: #DB7093;
-        width: 5em;
-    }
+  .my-submit-btn {
+    background-color: #db7093;
+    border-color: #db7093;
+    width: 5em;
+  }
 </style>
